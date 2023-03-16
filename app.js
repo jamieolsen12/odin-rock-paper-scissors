f22dea79b8977b986b34766b43a323c76e26027c
Jamie Olsen <jamieolsen12@gmail.com>
2023-03-16 14:30:23 +0700

    create playRound() function to take two parameters, userChoice and computerChoice and use nested switch statements to calculate the winner and return a string stating the winner and the win conditions

--- app.js
+++ app.js
@@ -6,6 +6,68 @@ function getComputerChoice() {
     return computerChoice
 }
 
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+let computerChoice = getComputerChoice()
+
+let userChoice = "paper"
+
+function playRound(userChoice, computerChoice) {
+    switch(userChoice) {
+        case "rock":
+            switch(computerChoice) {
+                case "rock":
+                    return "It's a tie! rock versus rock.";
+                case "paper":
+                    return "You lose! paper covers rock!";
+                case "scissors":
+                    return "You win! rock smashes scissors!";
+                default:
+                    return "error. computer choice invalid.";
+            }
+        case "paper":
+            switch(computerChoice) {
+                case "rock":
+                    return "You win! paper covers rock!";
+                case "paper":
+                    return "It's a tie! paper versus paper!";
+                case "scissors":
+                    return "You lose! scissors cut paper!";
+                default:
+                    return "error. computer choice invalid";
+            }
+        case "scissors":
+            switch(computerChoice) {
+                case "rock":
+                    return "You lose! rock smashes scissors!";
+                case "paper":
+                    return "You win! scissors cut paper!";
+                case "scissors":
+                    return "It's a tie. scissors versus scissors.";
+                default:
+                    return "error. computer choice invalid";
+            }
+        default:
+            return "error. user choice invalid.";
+    }
+}
+
+console.log(playRound(userChoice, computerChoice));
+
+userChoice = getComputerChoice();
+computerChoice = getComputerChoice();
+
+
+
+console.log(playRound(userChoice, computerChoice));
+
+userChoice = getComputerChoice();
+computerChoice = getComputerChoice();
+
+console.log(playRound(userChoice, computerChoice));
+
+userChoice = getComputerChoice();
+computerChoice = getComputerChoice();
+
+console.log(playRound(userChoice, computerChoice));
+
+userChoice = getComputerChoice();
+computerChoice = getComputerChoice();
